a25df109632ac7acb154fa832a5225b88af6228c
DevOFALL <44227207+gcsipai@users.noreply.github.com>
2025-11-03 05:19:22 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Ezt a fájlt a Bash szkript generálja a megadott jelszóval,
+// a $VPN_DOWNLOAD_PASSWORD változó értéke itt kerül beillesztésre futáskor.
+$PASSWORD = "[[A SCRIPT ÁLTAL BEILLESZTETT JELSZÓ]]"; 
+$DOWNLOAD_DIR = "ovpn_downloads/";
+$MIN_FILE_SIZE = 1000;
+$AUTHOR = "DevOFALL";
+$VERSION = "6.0";
+?>
